c499a88a1f5c777d2dc8e72025269a110b588b85
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2026-01-10 16:13:14 -0800

    fix linting errors

--- src/outcomes_working_group/datamodel/outcomes_working_group.py
+++ src/outcomes_working_group/datamodel/outcomes_working_group.py
@@ -1,5 +1,5 @@
 # Auto generated from outcomes_working_group.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-01-10T16:01:00
+# Generation date: 2026-01-10T16:02:11
 # Schema: outcomes_working_group
 #
 # id: https://w3id.org/EHS-Data-Standards/outcomes_working_group
