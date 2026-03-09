e697dc2eafaf8b3fb1739a410ec609dfe2d03065
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2026-03-09 10:10:43 -0700

    rebrand renaming

---
+++ src/soma/datamodel/__init__.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+from .soma import *  # noqa: F403
+
+THIS_PATH = Path(__file__).parent
+
+SCHEMA_DIRECTORY = THIS_PATH.parent / "schema"
+MAIN_SCHEMA_PATH = SCHEMA_DIRECTORY / "soma.yaml"
